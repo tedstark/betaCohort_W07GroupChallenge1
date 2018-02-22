a079cf0f453c63b1b02f06c5d1514a023adfe14b
Ted <ted.stark@gmail.com>
2018-02-21 23:11:03 -0700

    working app

--- build/resources/main/static/client.js
+++ build/resources/main/static/client.js
@@ -1,13 +1,10 @@
 function goToUserInfo() {
     var searchString = $("#inputName").val();
     window.location.href="/user/" + searchString;
-    console.log("User Page Ready")
 }
 function goToAllUsers() {
     window.location.href="/allusers";
-    console.log("All Users Page Ready")
 }
 function goHome() {
     window.location.href="/";
-    console.log("Home Page Ready")
 }
--- src/main/java/com/tedstark/blueberrymuffin/controller/IntroController.java
+++ src/main/java/com/tedstark/blueberrymuffin/controller/IntroController.java
@@ -1,6 +1,4 @@
 package com.tedstark.blueberrymuffin.controller;
-
-
 import com.tedstark.blueberrymuffin.data.UserRepository;
 import com.tedstark.blueberrymuffin.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +6,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import java.util.List;
 
 @Controller
@@ -20,16 +17,21 @@ public class IntroController {
     public String homePage() {
         return "index";
     }
-    @RequestMapping("/person/{name}")
-    public String getOneUser(@PathVariable String name, ModelMap modelMap) {
+    @RequestMapping("/user")
+    public String userPage() {
+        return "user";
+    }
+    @RequestMapping("/user/{name}")
+    public String userNamePage(@PathVariable String name, ModelMap modelMap) {
         User user = userRepository.findByName(name);
         modelMap.put("user", user);
         return "user";
     }
     @RequestMapping("/allusers")
-    public String getAllUsers(ModelMap modelMap){
-        List<User> allusers= userRepository.getAllUsers();
-        modelMap.put("allusers", allusers);
+    public String allUsersPage(ModelMap modelMap){
+        List<User> allUsers= userRepository.getAllUsers();
+        modelMap.put("allusers", allUsers);
         return "allusers";
     }
+
 }
